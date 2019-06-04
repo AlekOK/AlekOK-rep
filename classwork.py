ceebeee7dc8d305adbbcc6efa148e1d493e5ef6e
Antonenko Olexii <51174530+AlekOK@users.noreply.github.com>
2019-06-04 20:41:37 +0300

    add classwork

---
+++ classwork.py
@@ -0,0 +1,11 @@
+a = float(input("first number: "))
+b= float(input("second number: "))
+if a > b:
+    print("first number is max")
+elif a < b:
+    print("second number is max")
+else:
+    print("a and b are equal")
+            
+
+   
