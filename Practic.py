f9a117724ba7e9bd4084138c7b956e0fde3def37
Antonenko Olexii <51174530+AlekOK@users.noreply.github.com>
2019-06-04 16:40:36 +0300

    add homework

---
+++ Practic.py
@@ -0,0 +1,18 @@
+value_int = 10
+value_float = 8.4
+value_str = "No"
+
+big_int = value_int*3.5
+
+value_float -= 1
+
+value_int = value_int/value_float
+
+big_int= big_int/value_float
+
+value_str = value_str*2 + "Yes"*3
+
+print(value_float)
+print(value_str)
+print(big_int)
+print(value_int)
